f47a98608a2e8181c9bb51a0589fb58ccf6f47bf
Srilekhya Turlapati <srilekhya.turlapati@gmail.com>
2021-01-20 23:29:33 +0530

    gtest | tests added

--- Range/gtest/TestRange.cpp
+++ Range/gtest/TestRange.cpp
@@ -4,13 +4,28 @@
 
 using namespace std;
 
-TEST(RangeTest, Simple) {
-    ASSERT_TRUE(1 == 1);
+TEST(RangeTest, Contains_Item) {
+    Range rng(1, 5);
+    EXPECT_EQ(rng.contains(3), true);
+    EXPECT_EQ(rng.contains(2), true);
+    EXPECT_EQ(rng.contains(-3), false);
+    EXPECT_EQ(rng.contains(8), false);
 }
 
-TEST(RangeTest, Contains_Item) {
-    ASSERT_TRUE(Range(1, 5).contains(3) == true);
-    ASSERT_TRUE(Range(1, 5).contains(2) == true);
+TEST(RangeTest, Overlaps_Range) {
+    Range rng(3, 7);
+    EXPECT_EQ(rng.overlaps(Range(4, 8)), true);
+    EXPECT_EQ(rng.overlaps(Range(1, 5)), true);
+    EXPECT_EQ(rng.overlaps(Range()), false);
+    EXPECT_EQ(rng.overlaps(Range(2)), false);
+}
+
+TEST(RangeTest, Is_Sub_Range) {
+    Range rng(2, 6);
+    EXPECT_EQ(rng.is_sub_range(Range(1, 8)), true);
+    EXPECT_EQ(rng.is_sub_range(Range(7)), true);
+    EXPECT_EQ(rng.is_sub_range(Range()), false);
+    EXPECT_EQ(rng.is_sub_range(Range(2)), false);
 }
 
 int main(int argc, char* argv[]) {
